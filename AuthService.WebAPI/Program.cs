b0439ed536c34268c4f0d88a8c28141fc3874fc0
libelola <fabriciod08@gmail.com>
2025-11-03 00:32:05 -0300

    Applying authentication and testing with a dumb route

--- AuthService.WebAPI/Program.cs
+++ AuthService.WebAPI/Program.cs
@@ -1,6 +1,9 @@
 using AuthService.Application.DTOs;
 using AuthService.Application.Interfaces;
 using AuthService.Infra.IoC;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+            )
+        };
+    });
 
+builder.Services.AddAuthorization();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -20,6 +40,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
@@ -27,9 +49,36 @@ app.UseHttpsRedirection();
 // Minimal API endpoint
 app.MapPost("/token", async (LoginDTO loginDto, ILoginAppService loginService) =>
 {
-    
     var access_token = await loginService.AuthenticateAsync(loginDto);
     return Results.Ok(new { access_token });
 });
+
+app.MapGet("/validate", (ClaimsPrincipal user) =>
+{
+    if (user.Identity is not null && user.Identity.IsAuthenticated)
+        return Results.Ok(new { message = "Token is valid." });
+    return Results.Unauthorized();
+})
+.RequireAuthorization(); ;
+
+
+app.MapGet("/random-wheather", () =>
+{
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    var rng = new Random();
+    var weatherForecasts = Enumerable.Range(1, 5).Select(index => new
+    {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = summaries[rng.Next(summaries.Length)]
+    })
+    .ToArray();
+
+    return Results.Ok(weatherForecasts);
+}).RequireAuthorization();
 
 app.Run();
